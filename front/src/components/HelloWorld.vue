baaf205f8258508e080cc29b2bb195418e16e1cd
Danielcalderon <daniel.calderon@datadoghq.com>
2021-10-06 20:24:05 -0400

    update page

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="main-grid">
+    <h1>{{ msg2 }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+     <input v-model="message" placeholder="edit me">
+     <button @click="doSomething">I'm a button</button>
+     
+    
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  }, 
+  data() {
+    return{
+      msg2:'hello buddy'
+    }
+  },
+  methods: {
+    async doSomething(){
+      try {
+            const response = await axios.get(
+              "http://jsonplaceholder.typicode.com/posts"
+            );
+            // JSON responses are automatically parsed.
+            // this.posts = response.data;
+            console.log("its working")
+            console.log(response.data)
+          } catch (error) {
+            console.log(error);
+          }
+    }
   }
 }
 </script>
@@ -54,5 +56,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.main-grid {
+display: grid;
+grid-row-gap:50px; 
 }
 </style>
