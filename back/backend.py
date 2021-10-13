eddb6003f96320b5dadc27578c180e03517222c7
Duncan Harvey <duncan.harvey@datadoghq.com>
2021-10-13 19:09:09 -0400

    cleans up backend

--- back/backend.py
+++ back/backend.py
@@ -9,35 +9,18 @@ from spotipy.oauth2 import SpotifyClientCredentials
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
-
     # accepts: query_string in JSON of request body
     # returns: id, song_title, artist, album in JSON
-    @app.route('/search')
+
+    @app.route('/search', methods=["GET"])
     def search():
-        # text = request.args.get['text']
+        print(request.json, file=sys.stderr)
         text = "call me maybe"
         print(text, file=sys.stderr)
         spotify = spotipy.Spotify(
