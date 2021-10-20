84b422038c5bf258680a22f86fe096688aa887dd
bradleyjay <bradleyjshields@gmail.com>
2021-10-20 19:19:13 -0400

    Song search returns list of search results.

--- back/backend.py
+++ back/backend.py
@@ -4,12 +4,13 @@ import json
 from flask import Flask, request
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-
+    CORS(app)
     # a simple page that says hello
     @app.route("/hello")
     def hello():
@@ -18,19 +19,19 @@ def create_app(test_config=None):
     # accepts: query_string in JSON of request body
     # returns: id, song_title, artist, album in JSON
 
-#     curl -X POST 0.0.0.0/search -H 'Content-Type: application/json' -d '{"query_string":"freebird","limit":7}'
+    #     curl -X POST 0.0.0.0/search -H 'Content-Type: application/json' -d '{"query_string":"freebird","limit":7}'
     def search_result_parsing(results):
         test_names_arr = map(
-                            lambda x: {
-                                "song_name": x["name"],
-                                "song_id": x["id"],
-                                "album_name": x["album"]["name"],
-                                "artist_name": x["artists"][0]["name"],
-                                "duration": x["duration_ms"],
-                                "img_link": x["album"]["images"][0]["url"]
-                            },
-                            results["tracks"]["items"]
-                         )
+            lambda x: {
+                "song_name": x["name"],
+                "song_id": x["id"],
+                "album_name": x["album"]["name"],
+                "artist_name": x["artists"][0]["name"],
+                "duration": x["duration_ms"],
+                "img_link": x["album"]["images"][0]["url"],
+            },
+            results["tracks"]["items"],
+        )
         test_names_arr = json.dumps(list(test_names_arr))
         print(test_names_arr, file=sys.stderr)
         return test_names_arr
@@ -39,20 +40,16 @@ def create_app(test_config=None):
     def search():
         print(request.json, file=sys.stderr)
         if "query_string" not in request.json or request.json["query_string"] == "":
-            return 'bad request!', 400
+            return "bad request!", 400
         else:
             text = request.json["query_string"]
         limit = request.json["limit"] if "limit" in request.json else 5
         print(text, file=sys.stderr)
         print("limit: " + str(limit))
-        spotify = spotipy.Spotify(
-            client_credentials_manager=SpotifyClientCredentials())
+        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
         # https://spotipy.readthedocs.io/en/2.19.0/#spotipy.client.Spotify.search
         results = spotify.search(q=text, type="track", limit=limit)
         return search_result_parsing(results)
 
     return app
-
-
-
--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -2,52 +2,50 @@
   <div class="main-grid">
     <h1>{{ msg2 }}</h1>
     <div>
-     <input v-model="spotify_body" placeholder="edit me">
-     <button @click="doSomething">I'm a button</button>
-      <p>Message is: {{ spotify_body }}</p>     
-    
+      <input v-model="spotify_body" placeholder="edit me" />
+      <button @click="doSomething">I'm a button</button>
+      <p>Message is: {{ spotify_body }}</p>
     </div>
-   
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }, 
+    msg: String,
+  },
   data() {
-    return{
-      msg2:'hello buddy',
-      spotify_body: ''
-    }
+    return {
+      msg2: "hello buddy",
+      spotify_body: "",
+    };
   },
   methods: {
-    async doSomething(){
+    async doSomething() {
       try {
-            const response = await 
-              axios.post('/song', {
-                query_string: this.$data.spotify_body,
-                limit: 7
-              })
-              .then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-            // JSON responses are automatically parsed.
-            // this.posts = response.data;
-            console.log(this.$data.spotify_body)
-            console.log(response)
-          } catch (error) {
+        const response = await axios
+          .post("http://0.0.0.0/search", {
+            query_string: this.$data.spotify_body,
+            limit: 7,
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
             console.log(error);
-          }
-    }
-  }
-}
+          });
+        // JSON responses are automatically parsed.
+        // this.posts = response.data;
+        console.log(this.$data.spotify_body);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -68,7 +66,7 @@ a {
 }
 
 .main-grid {
-display: grid;
-grid-row-gap:50px; 
+  display: grid;
+  grid-row-gap: 50px;
 }
 </style>
