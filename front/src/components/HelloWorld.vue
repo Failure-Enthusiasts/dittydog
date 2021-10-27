42c3912cd7b2415b4db620bef8be3c01a3a13f01
Meghan Maloy <meghan.maloy@datadoghq.com>
2021-10-27 18:40:43 -0400

    search results show up on page

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -5,7 +5,10 @@
       <input v-model="spotify_body" placeholder="edit me" />
       <button @click="doSomething">I'm a button</button>
       <p>Message is: {{ spotify_body }}</p>
-      <p id="test123">Search Result: {{ doSomething() }}</p>
+      <p v-for="search_result in search_results" :key="search_result.song_id">
+        {{ search_result.song_name }} -
+        {{ search_result.artist_name }}
+      </p>
     </div>
   </div>
 </template>
@@ -22,30 +25,25 @@ export default {
     return {
       msg2: "hello buddy",
       spotify_body: "",
+      search_results: "",
     };
   },
   methods: {
-    async doSomething() {
+    doSomething: async function() {
       try {
         const response = await axios
           .post("http://0.0.0.0/search", {
             query_string: this.$data.spotify_body,
             limit: 7,
           })
-          // .then(function (response) {
-          //   console.log(response);
-          //   search_result = response;
-          // })
           .catch(function (error) {
             console.log(error);
           });
-        // JSON responses are automatically parsed.
-        // this.posts = response.data;
-        console.log(this.$data.spotify_body);
-        console.log(response);
-        document.querySelector("#test123").innerText =
-          response.data[1].song_name; // #FIXME: Can we find a more Vue way to do this?
-        return response.data;
+        console.log("RESPONSE")
+        console.log(response.data)
+        console.log("SEARCH TERM: " + this.$data.spotify_body)
+        this.search_results = response.data
+        return response.data
       } catch (error) {
         console.log(error);
       }
