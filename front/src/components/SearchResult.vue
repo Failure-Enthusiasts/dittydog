b5ba260b428c9a11dd30fdd163a32f8550ecdd07
cahillsf <stephen.cahill@datadoghq.com>
2021-12-15 19:32:49 -0500

    adding song_confirm func

--- front/src/components/SearchResult.vue
+++ front/src/components/SearchResult.vue
@@ -1,38 +1,40 @@
 <template>
   <div id="search-results">
     <SongItem
-        v-for="search_result in results_arr"
-        :key="search_result.song_id"
-        @click="song_confirm(search_result.song_id)"
-        v-bind:song_name="search_result.song_name"
-        v-bind:artist_name="search_result.artist_name"
-        v-bind:album_url="search_result.img_link"
+      v-for="search_result in results_arr"
+      :key="search_result.song_id"
+      @click="song_confirm(search_result.song_id)"
+      v-bind:song_name="search_result.song_name"
+      v-bind:artist_name="search_result.artist_name"
+      v-bind:album_url="search_result.img_link"
     >
     </SongItem>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import SongItem from "./SongItem";
 export default {
-  components: {SongItem},
+  components: { SongItem },
   props: {
     results_arr: Array,
   },
   methods: {
-    song_confirm: function (id) {
-      //hide the search results
-      console.log("pretend we added the song " + id);
+    song_confirm: async function (song_uri) {
       this.$emit("addsong");
-      // await axios
-      //   .post("http://0.0.0.0/confirm", {
-      //     song_id: id// (some way to grab the clicked-on song name goes here),
-      //   })
-      //   .catch(function (error) {
-      //     console.log(error);
-      //   });
-      return 
+      await axios
+        .post(
+          "http://localhost/confirm",
+          {
+            song_uri: song_uri, // (some way to grab the clicked-on song name goes here),
+          },
+          { withCredentials: true }
+        )
+        .catch(function (error) {
+          console.log(error);
+        });
     },
-  }
-}
+  },
+};
 </script>
