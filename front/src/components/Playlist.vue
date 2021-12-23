bbab4dcd740bf5135b3ad332ed24f1188f1f6934
cahillsf <stephen.cahill@datadoghq.com>
2021-12-22 20:30:39 -0500

    adding playlist component

---
+++ front/src/components/Playlist.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="playlist-results">
+    <SongItem
+      v-for="search_result in results_arr"
+      :key="search_result.song_id"
+      v-bind:song_name="search_result.song_name"
+      v-bind:artist_name="search_result.artist_name"
+      v-bind:album_url="search_result.img_link"
+    >
+    </SongItem>
+  </div>
+</template>
+
+<script>
+import SongItem from "./SongItem";
+export default {
+  components: { SongItem },
+  props: {
+    results_arr: Array,
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+#search-results{
+  border-style: solid;
+  border-radius: 10px;
+  border-width: 3px;
+}
+</style>
