98ddc0e88366180faddebb68a1ad02f4f22e044d
bradleyjay <bradleyjshields@gmail.com>
2022-04-13 20:24:22 -0400

    added lock icon for locked songs.

--- front/src/components/SongItem.vue
+++ front/src/components/SongItem.vue
@@ -4,6 +4,7 @@
     <p class="itemwrapper">
       {{ song_name }} -
       {{ artist_name }}
+      <img v-if="locked == true" src="@/assets/lock_icon.png" style=" padding-bottom:10px; padding-right:5px;">
     </p>
   </div>
 </template>
@@ -15,6 +16,7 @@ export default {
     artist_name: String,
     album_name: String,
     album_url: String,
+    locked: Boolean
   },
 }
 </script>
